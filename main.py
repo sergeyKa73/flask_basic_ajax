2c94e348f433377a2ee4fd0b0abaaf148daf0f10
Sergey <karandeev73@gmail.com>
2022-05-05 10:12:06 +0300

    feat: finished the task

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import json
+from time import time
+
 from flask import Flask
-from  flask import render_template
+from flask import jsonify
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.is_json:
+
+        if request.method == 'GET':
+            seconds = time()
+            return jsonify({'seconds': seconds})
+
+        if request.method == 'POST':
+            card_text = json.loads(request.data).get('text')  # .form or . json
+            new_text = f'I got: {card_text}'
+            return jsonify({'data': new_text})
+
     return render_template('index.html')
 
 
